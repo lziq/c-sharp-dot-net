fbb4f3e9b68dd10d81bbde1dcabfb3b9f5a6af0d
lziq <lziq@amazon.com>
2023-05-12 15:47:18 -0700

    Setup C# skeleton

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System.Runtime.InteropServices;
+using API.Data;
+using Microsoft.EntityFrameworkCore; 
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
+}); 
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
